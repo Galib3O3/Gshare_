58373f05a1132b78e5b755d176f1c1c549eb12d5
Md. Asadullah-Al-Galib <77722359+Galib3O3@users.noreply.github.com>
2023-08-11 16:39:45 +0600

    Update RegAction.php
    cmnt

--- LandingPage/RegAction.php
+++ LandingPage/RegAction.php
@@ -26,7 +26,7 @@ function sendMail($email,$verify_code){
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'gshare0303@gmail.com';                     //SMTP username
-        $mail->Password   = 'jrautwmlacekupxb';                               //SMTP password
+        $mail->Password   = 'jramlacpxb';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
